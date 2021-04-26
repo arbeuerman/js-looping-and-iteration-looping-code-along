93a0789b8e5713b064c9bfe353f97ff26d09a27a
Alexandra <arbeuerman@gmail.com>
2021-04-25 20:19:27 -0500

    commit tree 068c6847c6db32d9986922e03b52f70106796da1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, holiday)
+{
+    const new_array = []
+    for(let i = 0; i < names.length; i ++){
+        new_array.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+    };
+    return new_array;
+};
+
+function countDown(starting_number)
+{
+    let i = starting_number;
+    while(i >= 0){
+        console.log(i);
+        i--;
+    };
+};
